2fc7242c318738bd98b6b5fdc7678d3190297415
Harrison Kukel <HarrisonJamesKunkel@gmail.com>
2020-08-15 23:17:13 -0400

    add favicon, add root route and login route, and nav-bar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,9 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+//= require jquery
+//= require semantic-ui
+
+require("semantic-ui-sass")
+
+$(document).on("turbolinks:load", () => $(".ui.dropdown").dropdown());
